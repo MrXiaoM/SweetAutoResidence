de1bbe6756e361f81894c6c3c1dea72805fdc3c6
MrXiaoM <coolxiaom95@gmail.com>
2026-01-31 14:10:52 +0800

    update 1.0.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ buildscript {
 val base = top.mrxiaom.gradle.LibraryHelper(project)
 
 group = "top.mrxiaom.sweet.autores"
-version = "1.0.1"
+version = "1.0.2"
 val targetJavaVersion = 8
 val pluginBaseModules = base.modules.run { listOf(library, paper, actions, l10n) }
 val shadowGroup = "top.mrxiaom.sweet.autores.libs"
