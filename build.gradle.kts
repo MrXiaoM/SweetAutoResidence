c0ee92e0fc382cabcf0602b6e51cce9c6681c772
MrXiaoM <coolxiaom95@gmail.com>
2026-07-30 16:22:46 +0800

    update 1.0.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ buildscript {
 val base = LibraryHelper(project)
 
 group = "top.mrxiaom.sweet.autores"
-version = "1.0.4"
+version = "1.0.5"
 val targetJavaVersion = 8
 val pluginBaseModules = base.modules.run { listOf(library, paper, actions, l10n) }
 val shadowGroup = "top.mrxiaom.sweet.autores.libs"
@@ -44,7 +44,7 @@ dependencies {
     base.collectPluginHolders()
 
     implementation("com.github.technicallycoded:FoliaLib:0.4.4") { isTransitive = false }
-    implementation(base.depend.nbtapi)
+    implementation("de.tr7zw:item-nbt-api:2.16.0")
     for (artifact in pluginBaseModules) {
         implementation(artifact)
     }
