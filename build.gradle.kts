4e9c462e92cd504796e36cd889e29d2047c75caf
MrXiaoM <coolxiaom95@gmail.com>
2026-09-25 00:28:18 +0800

    upgrade dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,7 +43,7 @@ dependencies {
     base.library(LibraryHelper.adventure("4.25.0"))
     base.collectPluginHolders()
 
-    implementation("de.tr7zw:item-nbt-api:2.16.0")
+    implementation("de.tr7zw:item-nbt-api:2.16.1")
     for (artifact in pluginBaseModules) {
         implementation(artifact)
     }
